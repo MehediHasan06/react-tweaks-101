9cd9c519bb85a50c29c0bd48e1f6697f0a1ccfd9
MehediHasan0009 <mehedi.hasan@echologyx.com>
2022-09-16 23:56:00 +0600

    tempControl project is finished

--- src/components/project/tempControl/index.js
+++ src/components/project/tempControl/index.js
@@ -1,15 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./style.css";
 
 const TempControl = () => {
-  const [count, setCount] = useState(10);
-//useeffect
-
+  const [count, setCount] = useState(24);
+  const [tempColor, setTempColor] = useState("");
+  
+  const handleTempColor = () => {
+    if(count === 24){
+      setTempColor("#3EB489");
+    } else if(count<20){
+      setTempColor("#9AFEFF");
+    } else if(count>=20 && count<30){
+      setTempColor("#15317E");
+    } else {
+      setTempColor("red");
+    }
+  };
+  
+  useEffect(() => {
+    handleTempColor();
+  }, [count]);
 
   return (
     <div className="app-container">
       <div className="temperature-display-container">
-        <div className="temperature-display" style={{ backgroundColor: count > 0 ? 'red': 'blue'  }}>
+        <div className="temperature-display" style={{ backgroundColor: tempColor }}>
           {count}°C
         </div>
       </div>
